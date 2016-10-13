64a893229b27d51d8b135cdb8d1398fb33b8bd6d
anniekbh <anniekbh@gmail.com>
2016-10-13 07:14:35 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-    var contenedor = document.getElementById("contenedor");
-    var receptor = document.getElementById("receptor");
-    var casilla = document.getElementById("casillero");
-    var formulario = document.getElementById("formulario");
-    var inpuText = document.getElementById("inpuText");
-    var boton = document.getElementById("boton");
+var contenedor = document.getElementById("contenedor");
+var receptor = document.getElementById("receptor");
+var casilla = document.getElementById("casillero");
+var formulario = document.getElementById("formulario");
+var inpuText = document.getElementById("inpuText");
+var boton = document.getElementById("boton");
 
 window.addEventListener("load", function() {
 
@@ -18,7 +18,8 @@ window.addEventListener("load", function() {
         event.preventDefault();
         crearTitulo();
         inpuText.value="";
-    });   
+    }); 
+    
 });
 
 function crearTitulo(){
@@ -36,10 +37,40 @@ function crearTitulo(){
     bloque.setAttribute("class", "bloque")
     
     ancor.appendChild(textAncor);
-    tituloLista.insertBefore(ancor, tituloLista.children[0]);
     bloque.insertBefore(tituloLista, bloque.children[0]);
+    bloque.insertBefore(ancor, bloque.children[1]);
     receptor.appendChild(bloque);
     moverForm();
+
+    ancor.addEventListener("click", function(){
+        crearTarjeta();
+    });
+
+    function crearTarjeta(){
+    ancor.style.display = "none";
+    var nuevoTitulo = document.createElement("textarea");
+    var tarjeta = document.createElement("form");
+    var botoNuevo = document.createElement("input");
+
+    botoNuevo.setAttribute("type", "submit");
+    botoNuevo.setAttribute("value", "Añadir");
+
+    tarjeta.insertBefore(nuevoTitulo, tarjeta.children[0]);
+    tarjeta.insertBefore(botoNuevo, tarjeta.children[1]);
+
+    /*botonNuevo.addEventListener("click", function(e) {
+            e.preventDefault();
+            var textoTarjeta = nuevoTitulo.value;
+            var divTarjeta = document.createElement("div");
+            div.innerHTML = textoDiv;
+            tarjeta.style.display = "none";
+            ancor.parentElement.appendChild(div);
+            div.classList.add("divBorder");
+            div.parentElement.appendChild(enlace);
+            ancor.style.display = "block";
+
+        });*/
+    }
 }
 
 function moverForm(){
