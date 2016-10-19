1aab7d2421114f2462c0d2f387e19beee73f6e61
anniekbh <anniekbh@gmail.com>
2016-10-19 17:38:12 -0500

    agregando cambios

--- js/main.js
+++ js/main.js
@@ -8,35 +8,37 @@ var contador = 1;
 
 window.addEventListener("load", function() {
 
-    casilla.addEventListener("click", function(event) {
-        event.preventDefault();
-        this.style.display = "none";
-        formulario.style.display = "inline-block";
-        inpuText.focus();
-    });
+  casilla.addEventListener("click", function(event) {
+    event.preventDefault();
+    this.style.display = "none";
+    formulario.style.display = "inline-block";
+    inpuText.focus();
+  });
 
-    boton.addEventListener("click", function(event) {
-        event.preventDefault();
-        crearTitulo();
-        inpuText.value="";
-        moverForm();
-    }); 
+  boton.addEventListener("click", function(event) {
+    event.preventDefault();
+      crearTitulo(); 
+      inpuText.value="";
+      moverForm();
+  });
+   
 });
 
 function crearTitulo(){
-    formulario.style.display = "none";
+  formulario.style.display = "none";
 
-    var bloque = document.createElement("div");
-    var tituloLista = document.createElement("span");
-    var ancor = document.createElement("a");
-    var textAncor = document.createTextNode("Añadir una tarjeta...");
+  var bloque = document.createElement("div");
+  var tituloLista = document.createElement("span");
+  var ancor = document.createElement("a");
+  var textAncor = document.createTextNode("Añadir una tarjeta...");
 
-    tituloLista.innerText = inpuText.value;
+  tituloLista.innerText = inpuText.value;
             
-    ancor.setAttribute("href", "#");
-    ancor.setAttribute("class","ancor")
-    bloque.setAttribute("class", "bloque")
-    tituloLista.setAttribute("class", "tituloLista");
+  ancor.setAttribute("href", "#");
+  ancor.setAttribute("class","ancor")
+  bloque.setAttribute("class", "bloque")
+  bloque.setAttribute("draggable", "true");
+  tituloLista.setAttribute("class", "tituloLista");
     
     ancor.appendChild(textAncor);
     bloque.insertBefore(tituloLista, bloque.children[0]);
@@ -47,28 +49,10 @@ function crearTitulo(){
         event.preventDefault();
         crearTarjeta(ancor);
     });
-
-    bloque.addEventListener("dragover", dragSobreDiv, false);
-    bloque.addEventListener("dragleave", dragSalioDiv, false);
-    bloque.addEventListener("drop", manejarDrop, false);
-
-       function dragSobreDiv(e){
-            e.preventDefault();
-            this.classList.add("over");
-            return false;
-        };
-
-        function dragSalioDiv(e){
-            e.preventDefault();
-            this.classList.remove("over");
-        };
-
-        function manejarDrop(e) {
-            e.preventDefault();
-            var dato = e.dataTransfer.getData("text");
-            e.target.appendChild(document.getElementById(dato));
-        };
-
+    
+    //bloque.addEventListener("dragenter", dragEntroDiv);
+    bloque.addEventListener("dragover", dragSobreDiv);
+    //bloque.addEventListener("dragleave", dragSalioDiv);    
 };
 
 function crearTarjeta(ancor){
@@ -101,19 +85,43 @@ function crearTarjeta(ancor){
             ancor.style.display = "block";
             nuevaTarjeta.setAttribute("id", contador++);
 
-            nuevaTarjeta.addEventListener("dragstart", dragIniciado, false);
-                function dragIniciado(e) {
-                    //spanIn.style.backgroundColor = "blue";
-                    e.dataTransfer.setData("text", e.target.id);
-                };
+            nuevaTarjeta.addEventListener("dragstart", dragIniciado);
+            nuevaTarjeta.addEventListener("drop", manejarDrop);
+            nuevaTarjeta.addEventListener("dragend", dragTerminar);
         });
 };
 
+function dragIniciado(e) {
+    this.classList.add("bordeTarjeta")
+    e.dataTransfer.setData("text", e.target.id);
+};
+                
+function manejarDrop(e) {
+    var dato = e.dataTransfer.getData("text");
+    e.target.parentElement.insertBefore(document.getElementById(dato), e.target);
+};
+
+function dragTerminar(e) {
+    e.preventDefault();
+    this.classList.remove("bordeTarjeta")
+};
+
+//function dragEntro Div(e){
+    //this.classList.add("over");
+//};
+
+function dragSobreDiv(e){
+    e.preventDefault();
+};
+
+// function dragSalioDiv(e){
+    //this.classList.remove("over");
+//};
+
 function moverForm(bloque){
-    
     casilla.style.display = "inline";
-    
+   
     receptor.appendChild(casilla);
     formulario.remove();
     receptor.appendChild(formulario);
-};
+}
